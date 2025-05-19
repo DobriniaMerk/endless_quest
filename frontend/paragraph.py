8645938574b85fdcd6aafbcc79de90b132b48b75
KolesnikovaEk <Kolesnikova.Ek.21@yandex.ru>
2025-05-19 21:41:52 +0300

    inventory

--- frontend/paragraph.py
+++ frontend/paragraph.py
@@ -6,89 +6,11 @@ import json
 from db import get_db
 import parser
 
+from .locale_data import locale
+
 paragraph_bp = Blueprint("paragraph", __name__, url_prefix="/")
 
 langs = ["ru", "en"]
-
-locale = {
-    "ru": {
-        "show": {
-            "not_written": {
-                "title": "???",
-                "story": "События с этого момента покрываются туманом, и решительно ничего нельзя разобрать.",
-            },
-            "translate": {
-                "title": "???",
-                "story": "Что было дальше никто не знает, но люди знающие утверждают что такое уже происходило, "
-                "только тогда все было по-английски и никто ничего не понял. Если вы переводчик, "
-                "можете объяснить тем, кто не столь сведущ.",
-            },
-            "edit": "Изменить",
-            "add": "Добавить",
-            "goto": "Перейти",
-        },
-        "edit": {
-            "page-title": "Новая страница",
-            "title": "Заголовок",
-            "story": "Разворот",
-            "save": "Опубликовать",
-            "cancel": "Выбросить черновик",
-        },
-        "menu": {
-            "account": "Аккаунт",
-            "inventory": "Инвентарь",
-            "main_menu": "Меню",
-            "choose_design": "Выбрать оформление",
-            "history": "История",
-            "logout": "Выйти",
-            "login": "Войти",
-            "back": "Назад",
-        },
-        "theme": {
-            "light_theme": "Светлая тема",
-            "dark_theme": "Темная тема",
-            "my_theme": "Моя тема",
-        },
-    },
-    "en": {
-        "show": {
-            "not_written": {
-                "title": "???",
-                "story": "The story from this point is uncertain. Decide the outcome yourself, if you dare.",
-            },
-            "translate": {
-                "title": "???",
-                "story": "What happened next is unclear, but those who have the knowledge of Russian can transfer "
-                "the truth from over the Edge.",
-            },
-            "edit": "Edit",
-            "add": "Add",
-            "goto": "Goto",
-        },
-        "edit": {
-            "page-title": "Write new page",
-            "title": "Title",
-            "story": "Story",
-            "save": "Publish",
-            "cancel": "Better not",
-        },
-        "menu": {
-            "account": "Account",
-            "inventory": "Inventory",
-            "main_menu": "Menu",
-            "choose_design": "Choose a design",
-            "history": "History",
-            "logout": "Log out",
-            "login": "Log in",
-            "back": "Back",
-        },
-        "theme": {
-            "light_theme": "Light theme",
-            "dark_theme": "Dark theme",
-            "my_theme": "My theme",
-        },
-    },
-}
 
 # def new_paragraph(text: str) -> int:
 #     """Generate a new paragraph ID not yet in use"""
@@ -217,4 +139,7 @@ def account():
     else:
         username = "Гость"
         email = "не указано"
-    return render_template("account.html", username=username, email=email, locale=locale.get("ru"))
+    return render_template("account.html",
+                           username=username,
+                           email=email,
+                           locale=locale.get("ru"))
--- frontend/profile.py
+++ frontend/profile.py
@@ -1,18 +1,20 @@
 from flask import Blueprint, redirect, render_template, url_for
 from db import get_db
 
+from .locale_data import locale
+
 profile_bp = Blueprint("profile", __name__, url_prefix="/")
 
 langs = ["ru", "en"]
 
 @profile_bp.route("<lang>/<int:id>/profile", methods=["GET"])
 def show(id : int, lang : str):
-    if lang not in langs:
-        return redirect(url_for("profile.show", id=id, lang=langs[0]))
     db = get_db()
     raw = db.get_user_profile(id, lang)
     exists = bool(raw)
     return render_template(
             'profile.html',
-            variables=raw
+            variables=raw,
+            paragraph={"lang": lang, "id": id},
+            locale=locale.get(lang, locale["ru"])
             )
