d51096ece656d5685daaffc7974014a3b4cbf72b
This laptop owner <merkurev.dd@phystech.edu>
2025-05-19 21:08:15 +0300

    Fixes

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+
+from frontend import profile
 from .auth import *
 from .paragraph import *
 from .moderation import *
 from .guest import *
+from .profile import *
 
 def register_blueprints(app: Flask) -> None:
     """
@@ -15,3 +18,4 @@ def register_blueprints(app: Flask) -> None:
     app.register_blueprint(paragraph_bp)
     app.register_blueprint(moderation_bp)
     app.register_blueprint(guest_bp)
+    app.register_blueprint(profile_bp)
