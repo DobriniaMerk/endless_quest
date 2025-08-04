172778b6372eeefdf0eae570968ab5d1bde8fad7
This laptop owner <merkurev.dd@phystech.edu>
2025-08-04 16:33:00 +0300

    Error handling system + bugfixes

--- parser/parser.py
+++ parser/parser.py
@@ -1,20 +1,33 @@
 import lark
 from flask import current_app
+from urllib.parse import urlencode
+from enum import Enum
 
 GETVAR_CB = None
 GRAMMAR = None
 
 
+class Vartype(Enum):
+    ANY=1
+    STRING=1
+    INT=2
+    BOOL=3
+
+
 def process_page(text: str, variable_getter) -> str:
     """
     Evaluates all expressions in text and returns HTML
-    Args: Text to process, user id to get variables from
+    Args: Text to process, callback to get variables from
     Returns: HTML code of a processed page
     """
     global GETVAR_CB
     GETVAR_CB = variable_getter
     processed = ""
-    root = parse(text)
+    try:
+        root = parse(text)
+        print(root)
+    except Exception as e:
+        return str(e)
     for node in root.children:
         processed += process(node)
 
@@ -39,35 +52,64 @@ def parse(text: str) -> lark.tree.Tree:
     """
     Parses given code to parse tree
     Args: Text to parse
+
+    Throws: Inherited from ParserError if syntax is incorrect.
     Returns: Root node of parse tree
+
     """
     parser = lark.Lark(load_grammar())
-    return parser.parse(text)
+    try:
+        return parser.parse(text)
+    except lark.UnexpectedInput as u:
+        exc = u.match_examples(parser.parse, {
+            MissingDestination: ["((exmpl;))",
+                                 "((exmpl))"],
+            UnclosedBlock: ["((example; 1",
+                            "((t;1))((+flag",
+                            # "(?"
+                            "(?flag",
+                            # "(?flag?) (!",
+                            "(?flag?) (!flag",
+                            # "(%",
+                            "(%var",],
+            EmptyVarblock: ["((exmpl; 1))(())"]
+        }, use_accepts=True)
+
+        if not exc:
+            raise
+        raise exc(u.get_context(text), u.line, u.column) from u
+
 
 
 def process(node: lark.tree.Tree) -> str:
     """
     Processes one top-level node and returns it's html
+
+    Throws: Exceptions inherited from MarkdownError. Should be catched and displayed instead of text.
+    Returns: Resulting html after markdown evaluation.
     """
-    match node.data:
-        case "text":
-            return str(node.children[0])
-        case "subst":
-            return eval_subst(node)
-        case "if":
-            return eval_if(node)
-        case "link":
-            return parse_link(node)
-    raise UnknownNode(node.data)
+    try:
+        match node.data:
+            case "text":
+                ret = str(node.children[0])
+                return ret
+            case "newline":
+                return str(node.children[0])
+            case "subst":
+                return eval_subst(node)
+            case "if":
+                return eval_if(node)
+            case "link":
+                return parse_link(node)
+        raise UnknownNode("Unknow node: " + node.data + ". This is a site error, please report to developers.")
+    except MarkdownError as e:
+        return '<p style="color: red>"' + str(e) + '</p>'
 
 
 def eval_if(node: lark.tree.Tree) -> str:
     """
     Evaluates 'if' block
     """
-    if node.data != "if":
-        raise ValueError
-
     ret = ""
     if comp_eval(node.children[0]):
         for nd in node.children[1].children:
@@ -86,35 +128,53 @@ def eval_if(node: lark.tree.Tree) -> str:
     return ""
 
 
-def eval_expr(node: lark.tree.Tree) -> int:
+def eval_expr(node: lark.tree.Tree, expected_type: Vartype = Vartype.ANY) -> str | int | bool:
     """
-    Evaluates expression and returns it's result as int.
+    Evaluates expression and returns it's result as expected_type requires.
 
-    Returns: int
-    Throws: VariableNotSetError if at least one variable is not set
+    Returns: str
+    Throws: VariableError if encountered variable problems inside.
     """
     match node.data:
         case "expr":
-            return eval_expr(node.children[0])
+            ret = eval_expr(node.children[0])
+        case "string":
+            ret = str(node.children[0])[1:-1]
         case "sum":
-            return eval_expr(node.children[0]) + eval_expr(node.children[1])
+            ret = eval_expr(node.children[0], Vartype.INT) + eval_expr(node.children[1], Vartype.INT)
         case "diff":
-            return eval_expr(node.children[0]) - eval_expr(node.children[1])
+            ret = eval_expr(node.children[0], Vartype.INT) - eval_expr(node.children[1], Vartype.INT)
         case "mult":
-            return eval_expr(node.children[0]) * eval_expr(node.children[1])
+            ret = eval_expr(node.children[0], Vartype.INT) * eval_expr(node.children[1], Vartype.INT)
         case "div":
-            return eval_expr(node.children[0]) // eval_expr(node.children[1])
+            ret = eval_expr(node.children[0], Vartype.INT) // eval_expr(node.children[1], Vartype.INT)
         case "number":
-            return int(str(node.children[0]))
+            ret = int(str(node.children[0]))
         case "neg":
-            return -eval_expr(node.children[0])
+            ret = -eval_expr(node.children[0], Vartype.INT)
         case "var":
             varname = str(node.children[0])
             try:
-                return int(getvar(varname))
-            except KeyError as e:
-                raise VariableNotSetError(varname) from e
-    raise UnknownNode(node.data)
+                ret = getvar(varname)
+            except VariableNotSetError:
+                if expected_type == Vartype.STRING:
+                    return ""
+                elif expected_type == Vartype.INT:
+                    return 0
+                elif expected_type == Vartype.BOOL:
+                    return False
+        case _:
+            raise UnknownNode(node.data)
+
+    if expected_type == Vartype.STRING:
+        return str(ret)
+    elif expected_type == Vartype.BOOL:
+        return bool(ret)
+    elif expected_type == Vartype.INT:
+        try:
+            return int(ret)
+        except ValueError as e:
+            raise VariableError(f"{ret} is not a number") from e
 
 
 def eval_subst(node: lark.tree.Tree) -> str:
@@ -122,32 +182,23 @@ def eval_subst(node: lark.tree.Tree) -> str:
     Does expression substitution, evauating expr inside
     and returning default value is eval_expr failed
     """
-    try:
-        return str(eval_expr(node.children[0]))
-    except VariableNotSetError:
-        if len(node.children) > 1:
-            return str(node.children[1])
-        return ""  # maybe raise so eval_expr could insert some error code?
+    return eval_expr(node.children[0], Vartype.STRING)
 
 
 def parse_link(node: lark.tree.Tree) -> str:
     """Turns parse tree node to link html code"""
     text = process(node.children[0])
-    link = str(node.children[1])
+    variables = {}
     if len(node.children) > 2:
-        variables = []
         for nd in node.children[2].children:
             match nd.data:
                 case "flag":
-                    variables.append(str(nd.children[0]) + "=true")
+                    variables[str(nd.children[0])] = "true"
                 case "unflag":
-                    variables.append(str(nd.children[0]) + "=")
+                    variables[str(nd.children[0])] = ""
                 case "set":
-                    variables.append(
-                        str(nd.children[0]) + "=" + str(eval_expr(nd.children[1]))
-                    )
-        link += "?" + "&".join(variables)
-    return f'<a href="{link}">{text}</a>'
+                    variables[str(nd.children[0])] = eval_expr(nd.children[1], Vartype.STRING)
+    return f'<a href="{str(node.children[1])}?{urlencode(variables)}">{text}</a>'
 
 
 def comp_eval(node: lark.tree.Tree) -> bool:
@@ -161,15 +212,12 @@ def comp_eval(node: lark.tree.Tree) -> bool:
         case "flagvar":
             try:
                 ret = bool(getvar(str(node.children[0])))
-            except KeyError:
+            except VariableNotSetError:
                 ret = False
             return ret
         case "comp":
-            try:
-                lhs = eval_expr(node.children[0])
-                rhs = eval_expr(node.children[2])
-            except VariableNotSetError:
-                return False
+            lhs = eval_expr(node.children[0], Vartype.INT)
+            rhs = eval_expr(node.children[2], Vartype.INT)
 
             comp = str(node.children[1])
             match comp:
@@ -193,12 +241,13 @@ def comp_eval(node: lark.tree.Tree) -> bool:
 
 def getvar(name: str) -> str:
     """
-    Gets variable value from database. If value is not present — throws.
-
     Returns: Requested variable value
-    Throws: KeyError if asked variable is not set
+    Throws: VariableNotSetError if variable is not set
     """
-    return GETVAR_CB(name)
+    try:
+        return GETVAR_CB(name)
+    except Exception:
+        raise VariableNotSetError(name + " is not set")
 
 
 class UnknownComparator(Exception):
@@ -211,20 +260,55 @@ class UnknownComparator(Exception):
         self.message = msg
 
 
-class UnknownNode(Exception):
+class MarkdownError(Exception):
+    """
+    Error while evaluating or parsing markdown. Should be
+    intercepted and printed instead paragraph's text.
+
+    """
+    pass
+
+
+class UnknownNode(MarkdownError):
     """
     Raised when node name is not matched.
     Does not expected to be raised under normal circumstances
     """
+    pass
 
-    def __init__(self, msg):
-        self.message = msg
-
-
-class VariableNotSetError(Exception):
+class VariableNotSetError(MarkdownError):
     """
     Raised when there is an unset variable in eval_expr
     """
+    pass
 
-    def __init__(self, msg):
-        self.message = msg
+class VariableError(MarkdownError):
+    """
+    Something wrong with variables.
+    """
+    pass
+
+class MarkdownSyntaxError(MarkdownError):
+    """
+    Raised when parser encountered something wrong.
+    """
+    pass
+
+
+class ParserError(Exception):
+    label = ""
+    def __str__(self):
+        context, line, column = self.args
+        return f"{self.label} encountered at line {line}.\n\n{context}"
+
+class UnclosedBlock(ParserError):
+    label = "Unclosed control block"
+
+class MissingDestination(ParserError):
+    label = "Link without destination paragraph number"
+
+# class NonexistentOperation(ParserError):
+#     label = "Operation that does not exist"
+
+class EmptyVarblock(ParserError):
+    label = "Empty variable assignment block"
